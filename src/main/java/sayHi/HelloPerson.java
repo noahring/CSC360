4b801fed1fc95daa23034f98a1b3a6dc7100f9c8
Noah Ring <noahring@Noahs-MacBook-Air.local>
2024-02-02 00:00:29 -0500

    first commit

---
+++ src/main/java/sayHi/HelloPerson.java
@@ -0,0 +1,13 @@
+package sayHi;
+
+public class HelloPerson
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ src/main/java/sayHi/UnderAgeException.java
@@ -0,0 +1,28 @@
+package sayHi;
+
+public class UnderAgeException extends Exception
+{
+
+	private static final long serialVersionUID = -7990395458354141240L;
+
+	/**
+	 * 
+	 */
+	public UnderAgeException()
+	{
+		super("Parent is too Young");
+
+	}
+
+	/**
+	 * @param message
+	 */
+	public UnderAgeException(String message)
+	{
+		super(message);
+	}
+
+	
+	
+	
+}
